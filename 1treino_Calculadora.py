093073ab30faddce2f77e4ba93c43097529ed2a6
joaoRanulfoDatascience <joaouninove@hotmail.com>
2026-02-28 21:33:16 -0300

    ok

---
+++ 1treino_Calculadora.py
@@ -0,0 +1,14 @@
+# Treino de python
+
+# Fazendo uma calculadora para o problema 
+
+# Juliana vendeu seu patinete a 210 reais, o que era 70% do que ela havia pago.
+# Por quanto ela deveria ter vendido para obter um lucro de 15%?
+
+def calcular_venda_com_lucro(valor_venda_atual, porcentagem_atual, lucro_desejado):
+    custo = (valor_venda_atual* 100) / porcentagem_atual #Descobrir o custo original
+    valor_final = custo * (1 + (lucro_desejado/100))
+    return valor_final
+
+# Testando 
+print(f"Valor ideal: R$ {calcular_venda_com_lucro(210,70,15)}")
